0880f9b814b4a1342b645afa74e45ff3ba1bdfaf
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-17 10:14:25 +0100

    Refactor test to incorporate beforeEach

--- test/gameTest.js
+++ test/gameTest.js
@@ -1,10 +1,16 @@
 describe('game', function(){
+
+  var game
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
   it('can calculate if the game is a draw', function(){
-    var game = new Game()
     expect(game.result("Rock", "Rock")).toEqual('Its a draw!')
   })
+
   it('can calculate Rock beats Scisccors', function(){
-    var game = new Game()
     expect(game.result("Rock", "Scissors")).toEqual('Player 1 wins!')
   })
 })
