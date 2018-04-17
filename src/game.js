0526b1b66c2aa35d62a36484e243263c629e4a12
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-17 09:55:58 +0100

    Refactor draw condition out to separate function

--- src/game.js
+++ src/game.js
@@ -2,6 +2,10 @@ function Game(player){
 }
 
 Game.prototype.result = function(player1, player2){
+  return this._matchDraw(player1, player2)
+}
+
+Game.prototype._matchDraw = function(player1, player2){
   if (player1 === player2){
     return "Its a draw!"
   }
