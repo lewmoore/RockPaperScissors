c87d59981acd902a7f7e957a5aec501d63517bc7
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-17 09:25:58 +0100

    Remove redundant player test

--- test/playerTest.js
+++ test/playerTest.js
@@ -1,9 +1,4 @@
 describe('Player', function(){
-  // it('Player can choose their move', function() {
-  //   var player = new Player()
-  //   expect(player.move('Rock')).toEqual('Rock')
-  // })
-
   it('Player move is stored', function(){
     var player = new Player('Rock')
     expect(player.currentMove).toEqual('Rock')
