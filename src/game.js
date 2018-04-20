03b0d776044352e2436e217ce582e11b89c2c6d7
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-20 10:00:56 +0100

    translate game.js to es6 class layout

--- src/game.js
+++ src/game.js
@@ -1,44 +1,47 @@
-function Game(player1, player2){
-}
+class Game{
+  constructor(player1, player2){
 
-Game.prototype.result = function(player1, player2){
-  if (this._paperWin(player1, player2)) {
-    return this._paperWin(player1, player2)
-  } else if (this._rockWin(player1, player2)) {
-    return this._rockWin(player1, player2)
-  } else if (this._scissorWin(player1, player2)) {
-    return this._scissorWin(player1, player2)
-  } else if (this._matchDraw(player1, player2)) {
-    return this._matchDraw(player1, player2)
   }
-}
 
-Game.prototype._matchDraw = function(player1, player2){
-  if (player1.currentMove === player2.currentMove){
-    return "Its a draw!"
+  result(player1, player2){
+    if (this._paperWin(player1, player2)) {
+      return this._paperWin(player1, player2)
+    } else if (this._rockWin(player1, player2)) {
+      return this._rockWin(player1, player2)
+    } else if (this._scissorWin(player1, player2)) {
+      return this._scissorWin(player1, player2)
+    } else if (this._matchDraw(player1, player2)) {
+      return this._matchDraw(player1, player2)
+    }
   }
-}
 
-Game.prototype._rockWin = function(player1, player2){
-  if (player1.currentMove == "Rock" && player2.currentMove == "Scissors"){
-    return "Player 1 wins!"
-  } else if (player1.currentMove == "Scissors" && player2.currentMove == "Rock"){
-    return "Player 2 wins!"
+  _matchDraw(player1, player2){
+    if (player1.currentMove === player2.currentMove){
+      return "Its a draw!"
+    }
   }
-}
 
-Game.prototype._scissorWin = function(player1, player2){
-  if (player1.currentMove == 'Scissors' && player2.currentMove == 'Paper'){
-    return "Player 1 wins!"
-  } else if (player1.currentMove == "Paper" && player2.currentMove == "Scissors"){
-    return "Player 2 wins!"
+  _rockWin(player1, player2){
+    if (player1.currentMove == "Rock" && player2.currentMove == "Scissors"){
+      return "Player 1 wins!"
+    } else if (player1.currentMove == "Scissors" && player2.currentMove == "Rock"){
+      return "Player 2 wins!"
+    }
   }
-}
 
-Game.prototype._paperWin = function(player1, player2){
-  if (player1.currentMove == 'Paper' && player2.currentMove == 'Rock'){
-    return 'Player 1 wins!'
-  } else if (player1.currentMove == "Rock" && player2.currentMove == "Paper"){
-    return "Player 2 wins!"
+  _scissorWin(player1, player2){
+    if (player1.currentMove == 'Scissors' && player2.currentMove == 'Paper'){
+      return "Player 1 wins!"
+    } else if (player1.currentMove == "Paper" && player2.currentMove == "Scissors"){
+      return "Player 2 wins!"
+    }
+  }
+
+  _paperWin(player1, player2){
+    if (player1.currentMove == 'Paper' && player2.currentMove == 'Rock'){
+      return 'Player 1 wins!'
+    } else if (player1.currentMove == "Rock" && player2.currentMove == "Paper"){
+      return "Player 2 wins!"
+    }
   }
 }
